c79d1e8a66315caad6b330b0c8144492b85f4250
afik <melinjomail@gmail.com>
2025-08-17 14:26:08 +0700

    perubahan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+$routes->get('Home', 'Home::index');
+$routes->get('Tes', 'Tes::index');
 
 
---
+++ app/Views/tes.php
@@ -0,0 +1,5 @@
+tes
+
+    <a href="<?= base_url('Home') ?>">Welcome</a>
+    <a href="<?= base_url('/Home') ?>">Welcome</a>
+    <a href="<?= base_url('/Home/') ?>">Welcome</a>
